7f4b7ee1c8cc73a23c905a9d76bede62eed42d01
reikyDM <reikydemanto@gmail.com>
2020-05-10 22:19:01 +0700

    bab 2 selesai

---
+++ app/src/main/java/com/example/smk_coding_tugas_2/MyFriend.kt
@@ -0,0 +1,9 @@
+package com.example.smk_coding_tugas_2
+
+data class MyFriend (
+    val nama : String,
+    val kelamin : String,
+    val email : String,
+    val telp : String,
+    val alamat : String
+)
--- app/src/main/java/com/example/smk_coding_tugas_2/MyFriendsFragment.kt
+++ app/src/main/java/com/example/smk_coding_tugas_2/MyFriendsFragment.kt
@@ -5,8 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_my_friend.*
 
 class MyFriendsFragment : Fragment() {
+
+    lateinit var listTeman : ArrayList<MyFriend>
+    private fun simulasiDataTeman() {
+        listTeman = ArrayList()
+        listTeman.add(MyFriend("Fakhry", "Laki-laki", "fakhry@smkcoding.id", "081123123123",
+            "Malang"))
+        listTeman.add(MyFriend("Ahmad", "Laki-laki", "ahmad@smkcoding.id","085123123123", "Malang"))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +32,17 @@ class MyFriendsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView()
     }
-}
+
+    private fun tampilTeman() {
+        rv_listMyFriends.layoutManager = LinearLayoutManager(activity)
+        rv_listMyFriends.adapter = MyFriendAdapter(activity!!, listTeman)
+    }
+
+    private fun initView() {
+        simulasiDataTeman()
+        tampilTeman()
+    }
+
+    }
